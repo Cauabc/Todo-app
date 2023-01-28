6a363bb337071792ce2af131b3823cd05fe01e74
Cauabc <cauacardosoyt@gmail.com>
2023-01-27 21:05:39 -0300

    Basic template, body and theme toggler added

---
+++ src/components/ListContainer/ListContainer.jsx
@@ -0,0 +1,9 @@
+// const themeToggler = () => {
+//     theme === 'dark' ? setTheme('light') : setTheme('dark')
+//   }
+
+export const ListContainer = () => {
+    return(
+        <h1>eae tropa</h1>
+    )
+}
---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+    body: 'hsl(0, 0%, 98%)',
+    container: 'hsl(236, 33%, 92%)',
+    textColor: 'hsl(235, 19%, 35%)',
+    checked: 'hsl(233, 14%, 35%)',
+    active: 'hsl(220, 98%, 61%)',
+    hover: 'hsl(235, 19%, 35%)',
+    unHover: 'hsl(236, 9%, 61%)'
+}
+
+export const darkTheme = {
+    body: 'hsl(235, 21%, 11%)',
+    container: 'hsl(235, 24%, 19%)',
+    textColor: 'hsl(234, 39%, 85%)',
+    checked: 'hsl(233, 14%, 35%)',
+    active: 'hsl(220, 98%, 61%)',
+    hover: 'hsl(236, 33%, 92%)',
+    unHover: 'hsl(233, 14%, 35%)'
+}
